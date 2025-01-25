3c58bc3029150088ee7539f9a9695b36be131cf8
Binu Maka <binumaka9@gmail.com>
2025-01-25 19:48:00 +0545

    destintions

---
+++ src/pages/destinations.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import CategoryBar from "../components/categoryBar";
+import Navbar from '../components/NavBar';
+
+function Destinations () {
+    return (
+    <div>
+        <Navbar/>
+        <CategoryBar/>
+    </div>
+    );
+}
+
+export default Destinations;
