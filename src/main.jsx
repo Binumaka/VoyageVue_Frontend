a6a676e8ff7ebb7f7e45b44085a8d6fea7bd3830
Binu Maka <binumaka9@gmail.com>
2025-02-27 20:05:00 +0545

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import createRoot
-import App from './App';
-import './index.css'; // Ensure this file contains Tailwind's directives
-import { AuthProvider } from './context/AuthContext';
+import React from "react";
+import ReactDOM from "react-dom/client"; // Import createRoot
+import App from "./App";
+import "./index.css"; // Ensure this file contains Tailwind's directives
+import { AuthProvider } from "./private/context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+const root = ReactDOM.createRoot(document.getElementById("root")); // Use createRoot
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 );
-
